fa4ed8c037a3e6042a3fd4b197b1469f56c26068
wcornell <william.m.cornell@gmail.com>
2014-04-23 14:27:18 -0400

    v0.19 templatized DLList.h (first attempt)

--- DLList.h
+++ DLList.h
@@ -1,51 +1,54 @@
-// Forward declarations and prototypes used in the classes below
+#ifndef _DLLIST_H
+#define _DLLIST_H
 
+// Forward declarations and prototypes used in the classes below
 #include <iostream>
 #include <cstddef>
 
+template <typename T>
 class DLList;
+template <typename T>
+std::ostream& operator<< <T>(std::ostream& os, const DLList<T>& l);
 
-std::ostream& operator<<(std::ostream& os, const DLList& l);
-
-
-class Node {
+template <typename T>
+class Node<T> {
 	friend class DLList;
 public:
-	Node(int item, Node * next = NULL, Node * prev = NULL)
+	Node(T item, Node * next = NULL, Node * prev = NULL)
 		: item_(item), next_(next), prev_(prev) {}
 private:
-	int item_;
+	T item_;
 	Node * next_;
 	Node * prev_;
 };
 
-class DLList {
+class DLList<T> {
 public:
 	// Constructors
-	DLList()
+	DLList<T>()
 		: head(NULL), tail(NULL), nextiter(NULL), size(0) {}
-	DLList(const DLList& from);
+	DLList<T>(const DLList& from);
 
 	// Destructor
 	virtual ~DLList();
 
 	// Member functions
-	DLList& operator=(const DLList& from);
+	DLList<T>& operator=(const DLList<T>& from);
 	int length() const { return size; }
-	void append(const int& item);
-	void insert(int index, const int& item);
-	int pop(int index=-1);
-	int& operator[](int index);
+	void append(const T& item);
+	void insert(int index, const T& item);
+	T pop(int index=-1);
+	T& operator[](int index);
 	std::string str() const;
 	void resetForward() { nextiter = NULL; }
-	int next();
+	T next();
 
 private:
 	Node * head;
 	Node * tail;
 	Node * nextiter;
 	int size;
-	void allocCopy(const DLList& from);
+	void allocCopy(const DLList<T>& from);
 	void dealloc();
 	Node * _find(int index);
 	int _delete(int index);
@@ -59,3 +62,6 @@ class IndexError { };
 class StopIteration { };
 class DeallocError { };
 class AllocError { };
+
+#include "DLList.hpp"
+#endif
